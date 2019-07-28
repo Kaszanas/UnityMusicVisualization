570f39e36d52398b95414061441fc349b046961f
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2019-07-28 03:19:02 +0200

    Adding Unity Project

---
+++ AudioVisualization_1/Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+
+    AudioSource thisAudio;
+    public static float[] samples = new float[512];
+    public static float[] freqband = new float[8];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAudio = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        FrequencyBands();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        thisAudio.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void FrequencyBands()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+
+            float average = 0;
+            int samplecount = (int)Mathf.Pow(2, i) * 2;
+            
+            if (i == 7)
+            {
+                samplecount += 2;
+            }
+
+            for (int j = 0; j < samplecount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            freqband[i] = average * 10;
+
+
+        }
+
+
+
+    }
+
+
+}
---
+++ AudioVisualization_1/Assets/Scripts/ParamCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+
+    public int band;
+    public float startscale, scalemultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, (AudioPeer.freqband[band] * scalemultiplier) +startscale, transform.localScale.z);
+    }
+}
