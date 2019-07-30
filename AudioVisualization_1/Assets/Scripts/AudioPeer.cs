e685b0e713080528549f6086b2a7c2cdc2dbb332
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2019-07-30 04:33:59 +0200

    Renamed assets broke some stuff
    Decided to change approach to automatically create cube instances instead of manually place them in the scene.
    There is Error coming out as soon as the game starts.

--- AudioVisualization_1/Assets/Scripts/AudioPeer.cs
+++ AudioVisualization_1/Assets/Scripts/AudioPeer.cs
@@ -7,7 +7,8 @@ public class AudioPeer : MonoBehaviour
 {
 
     AudioSource thisAudio;
-    float[] samples = new float[512];
+    public static float[] samples = new float[512];
+    public static float[] samples8 = new float[8];
     float[] freqBand = new float[8];
     float[] buffer = new float[8];
     float[] bufferDecrease = new float[8];
@@ -20,6 +21,10 @@ public class AudioPeer : MonoBehaviour
     void Start()
     {
         thisAudio = GetComponent<AudioSource> ();
+        GetSpectrumAudioSource();
+        FrequencyBands();
+        CreateAudioBands();
+        BandBuffer();
     }
 
     // Update is called once per frame
@@ -27,51 +32,17 @@ public class AudioPeer : MonoBehaviour
     {
         GetSpectrumAudioSource();
         FrequencyBands();
-        BandBuffer();
         CreateAudioBands();
+        BandBuffer();
+        
     }
+
 
     void GetSpectrumAudioSource()
     {
         thisAudio.GetSpectrumData(samples, 0, FFTWindow.Blackman);
     }
 
-    void CreateAudioBands()
-    {
-        for (int i = 0; i < 8; i++)
-        {
-            if (freqBand[i] > freqBandHighest[i])
-            {
-                freqBandHighest[i] = freqBand[i];
-            }
-            audioBand [i] = (freqBand [i] / freqBandHighest [i]);
-            audioBandBuffer[i] = (buffer [i] / freqBandHighest [i]);
-        }
-
-    }
-
-
-    void BandBuffer()
-    {
-        for (int g = 0; g < 8; g++)
-        {
-
-            if (freqBand [g] > buffer [g])
-            {
-                buffer[g] = freqBand[g];
-                bufferDecrease[g] = 0.005f;
-            }
-
-
-            if (freqBand[g] < buffer[g])
-            {
-                buffer[g] -= bufferDecrease [g];
-                bufferDecrease[g] *= 1.2f;
-            }
-
-        }
-
-    }
 
     void FrequencyBands()
     {
@@ -82,7 +53,7 @@ public class AudioPeer : MonoBehaviour
 
             float average = 0;
             int samplecount = (int)Mathf.Pow(2, i) * 2;
-            
+
             if (i == 7)
             {
                 samplecount += 2;
@@ -96,7 +67,7 @@ public class AudioPeer : MonoBehaviour
 
             average /= count;
 
-            freqband[i] = average * 10;
+            freqBand[i] = average * 10;
 
 
         }
@@ -104,6 +75,53 @@ public class AudioPeer : MonoBehaviour
 
 
     }
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (freqBand[i] > freqBandHighest[i])
+            {
+                freqBandHighest[i] = freqBand[i];
+            }
+            audioBand[i] = (freqBand[i] / freqBandHighest[i]);
+            audioBandBuffer[i] = (buffer[i] / freqBandHighest[i]);
+        }
+
+    }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; g++)
+        {
+
+            if (freqBand[g] > buffer[g])
+            {
+                buffer[g] = freqBand[g];
+                bufferDecrease[g] = 0.005f;
+            }
+
+
+            if (freqBand[g] < buffer[g])
+            {
+                buffer[g] -= bufferDecrease[g];
+                bufferDecrease[g] *= 1.2f;
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
---
+++ AudioVisualization_1/Assets/Scripts/Dynamic8Bands.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic8Bands : MonoBehaviour
+{
+
+
+    public GameObject sampleCubePrefab;
+    GameObject[] cubePrefab = new GameObject[8];
+    public float maxScale8Bands;
+    public float cubeScale = 5f;
+    public bool useBuffer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+
+            // Creating Instance of a specified cube
+            GameObject instanceSampleCube = (GameObject)Instantiate(sampleCubePrefab);
+            instanceSampleCube.transform.position = this.transform.position;
+            instanceSampleCube.transform.parent = this.transform;
+
+            // Setting the name for instanced cubes
+            instanceSampleCube.name = "SampleCube8BandCube" + i;
+
+            // Transforming every instanced cube
+
+            // Creating as much cubes as it is the range of this loop
+            cubePrefab[i] = instanceSampleCube;
+            cubePrefab[i].transform.localScale = new Vector3(cubeScale, cubeScale, cubeScale);
+            cubePrefab[i].transform.position = new Vector3(cubeScale + i, 0, 0);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (cubePrefab != null)
+            {
+                if (useBuffer)
+                {
+                    cubePrefab[i].transform.localScale = new Vector3(cubeScale, cubeScale + AudioPeer.audioBandBuffer[i] * maxScale8Bands, cubeScale);
+                }
+
+                if (!useBuffer)
+                {
+                    cubePrefab[i].transform.localScale = new Vector3(cubeScale, cubeScale + AudioPeer.audioBand[i] * maxScale8Bands, cubeScale);
+                }
+                
+
+            }
+        }
+    }
+}
